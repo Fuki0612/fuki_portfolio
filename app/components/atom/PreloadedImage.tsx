b7db41016740fb2453b72cc0a20044c4e63c23ac
Fuu <fukimax0612@gmail.com>
2025-04-16 01:31:39 +0900

    画像表示高速化

--- app/components/atom/PreloadedImage.tsx
+++ app/components/atom/PreloadedImage.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import Image, { ImageProps } from 'next/image';
 
 export const PreloadedImage: React.FC<ImageProps> = (props) => {
-  return <Image {...props} />;
+  return <Image {...props} priority/>;
 };
